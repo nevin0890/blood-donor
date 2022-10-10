955a8b67955e0b0c5469d4aa93bd58650038b83f
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-10 07:18:49 +0530

    initial commit

---
+++ src/app/adddonor/adddonor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adddonor',
+  templateUrl: './adddonor.component.html',
+  styleUrls: ['./adddonor.component.css']
+})
+export class AdddonorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdddonorComponent } from './adddonor/adddonor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdddonorComponent
   ],
   imports: [
     BrowserModule,
